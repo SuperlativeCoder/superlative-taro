340e2fa2bb59bb8b186294e898bdc721f221f1db
ChangerHe <244737162@qq.com>
2018-11-27 20:39:33 +0800

    fix; constants

--- src/constants/weapp.js
+++ src/constants/weapp.js
@@ -5,5 +5,5 @@ export const isIOS = /ios/ig.test(systemInfo.system);
 export const isAndroid = /android/ig.test(systemInfo.system);
 
 export const STATUSBAR_HEIGHT = systemInfo.statusBarHeight;
-export const ANDROID_NAVHAR_HEIGHT = 48;
+export const ANDROID_NAVBAR_HEIGHT = 48;
 export const IOS_NAVBAR_HEIGHT = 44;
